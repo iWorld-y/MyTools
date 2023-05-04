981a5b6e6d2028135ccbe9494de36b04eafa425d
iWorld Jiang <caima12138@foxmail.com>
2023-05-04 20:15:12 +0800

    添加了

--- bash/alias.sh
+++ bash/alias.sh
@@ -3,18 +3,26 @@ alias lt='ll -t'
 alias myact='mamba activate'
 
 function mygit() {
+    # 检查是否提供了提交信息
     if [ -z "$1" ]; then
         echo "Please provide a commit message."
         return 1
     fi
 
+    # 获取最新代码并合并到本地仓库
     git pull origin main
 
+    # 检查是否有未提交的更改
     if [[ -n $(git status -s) ]]; then
+        # 如果有未提交的更改，则将它们添加到暂存区
         git add .
+        # 提交更改并添加提交信息
         git commit -m "$1"
+        # 推送更改到远程仓库
         git push origin main
     else
+        # 如果没有未提交的更改，则显示提示信息
         echo "No changes to commit."
     fi
 }
+
